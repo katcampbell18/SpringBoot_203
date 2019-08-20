2c84f1da26f3f68dd4fb715bd770cb845a6931ba
katcampbell18 <ktrncampbell@gmail.com>
2019-08-20 10:59:28 -0400

    added id attribute to User class

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
 public class User{
+    private long id;
     private String firstname;
     private String lastname;
     private String ssn;
     private String dob;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getFirstname() {
         return firstname;
     }
